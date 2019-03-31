70613fb46d36cda9b70ef2353976da549a9bd1ee
mennaanwer <menna.anwer.ma@gmail.com>
2019-03-31 13:30:14 +0200

    v-1.2

---
+++ script.js
@@ -0,0 +1,11 @@
+function openNav() {
+  document.getElementById("FullNav").style.width = "30%";
+ document.getElementById("window").style.marginLeft = "30%";	
+document.getElementById('btn').style.display='none';
+}
+
+function closeNav() {
+  document.getElementById("FullNav").style.width = "0%";
+ document.getElementById("window").style.marginLeft = "0%";
+document.getElementById('btn').style.display='';
+}
